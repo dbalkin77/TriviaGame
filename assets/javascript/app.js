88d9647c71c5fe204d3116f725b82a9fbf466256
Douglas Balkin <dbalkin77@gmail.com>
2017-06-04 23:22:57 -0500

    update to html and css finish button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,33 @@
 $(document).ready(function() {
     console.log( "ready!" );
 
-// var oneMinute = ; 
-
-// Timer
 function startTimer(duration, display) {
-    var start = Date.now(),
-        diff,
-        minutes,
-        seconds;
-    function timer() {
-        // get the number of seconds that have elapsed since 
-        // startTimer() was called
-        diff = duration - (((Date.now() - start) / 1000) | 0);
-
-        // does the same job as parseInt truncates the float
-        minutes = (diff / 60) | 0;
-        seconds = (diff % 60) | 0;
+    var timer = duration, minutes, seconds;
+    var __timer = setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds; 
+        display.textContent = minutes + ":" + seconds;
 
-        if (diff <= 0) {
-            // add one second so that the count down starts at the full duration
-            // example 05:00 not 04:59
-            start = Date.now() + 1000;
+        if (--timer < 0) {
+            clearInterval(__timer);
+            timer = duration;
         }
-    };
-    // we don't want to wait a full second before the timer starts
-    timer();
-    setInterval(timer, 1000);
+    }, 1000);
 }
-
-function stopTimer () {
-    window.clearTimeout(oneMinute)
-    alert('game over!');
-}
-
-// Starts timer when webpage loads
 
 window.onload = function () {
-        var oneMinute = 5 * 1,
+    var fiveMinutes = 60 * 1,
         display = document.querySelector('#time');
-    startTimer(oneMinute, display);
-        if (oneMinute === 0) {
-            stopTimer ();
-        }
+    startTimer(fiveMinutes, display);
 };
 
-
-});
-
 // Counters for Game
-var numberCorrect = 1;
-var numberIncorrect = 1;
+var numberCorrect = 0;
+var numberIncorrect = 0;
 
 // Keep track of correct and incorrect answers
 $(".correctAnswer").on("click", function () {
@@ -66,5 +38,11 @@ $(".correctAnswer").on("click", function () {
 $(".incorrectAnswer").on("click", function () {
     console.log(numberIncorrect++)
     return numberIncorrect;
+});
+
+$("#doneButton").on("click", function(){
+    $("#wrapper").empty();
+})
+
 });
 
